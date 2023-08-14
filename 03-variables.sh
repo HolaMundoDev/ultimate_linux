58e16ba5dc2b7c0d88d4015499ba773f97af5051
Gabriel Hernández <onedrako@gmail.com>
2023-08-14 13:45:48 -0600

    Clase 78. Variables

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+variable="Hola Mundo"
+numero=12
+flotante=12.5
+booleano=false
+
+echo variable # Esto imprime el texto "variable"
+echo $variable # Esto imprime Hola Mundo
+
+variable="Chanchito feliz"
+echo $variable $numero $booleano
