c8dd15e956f1ed765a477014e4b77b76bc56c388
Gabriel Hernández <onedrako@gmail.com>
2023-08-14 16:57:25 -0600

    Clase 89. Alcance

---
+++ 14-alcance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mensaje2="Mensaje 2"
+
+function holaMundo() {
+	mensaje="Hola Mundo!"
+	# local mensaje2="Hola Mundo 2!"
+	mensaje2="Hola Mundo 2!"
+}
+holaMundo
+
+echo $mensaje $mensaje2
