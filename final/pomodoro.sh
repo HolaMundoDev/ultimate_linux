e36bda89833bbbc15c627296495de81ab38d1388
Gabriel Hernández <onedrako@gmail.com>
2023-08-15 17:56:38 -0600

    Clase 107. Pomodoro

---
+++ final/pomodoro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+read -p "Ingresa el tiempo que va a durar el pomodoro (en segundos): " tiempo
+echo "Comenzando pomodoro! proximo descanso en $tiempo segundos."
+sleep $tiempo
+
+espeak -ves "El tiempo ha terminado, ahora puedes descansar."
+read -p "Presiona cualquier tecla para continuar" _
+
