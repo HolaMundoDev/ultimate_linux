44fb418f620f67bb5a5a17f15523dc8f6d6eea30
Gabriel Hernández <onedrako@gmail.com>
2023-08-15 14:29:55 -0600

    Clase 99. Continue y break

---
+++ flujo/10-continue-break.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+i=0
+echo "Primer buble"
+while [[ $i -lt 3 ]]; do
+	echo "1 - i vale $i"
+	i=$(($i+1))
+	continue
+	echo "2 - i vale $i"
+done
+
+i=0
+echo "Segundo bucle"
+while [[ $i -lt 3 ]]; do
+        echo "1 - i vale $i"
+        i=$(($i+1))
+	break
+        echo "2 - i vale $i"
+done
+
+i=0
+echo "Tercer bucle"
+while [[ $i -lt 3 ]]; do
+        echo "1 - i vale $i"
+        i=$(($i+1))
+	if [ $i -eq 2 ]; then
+		echo "cerrando bucle"
+		break
+	fi
+	echo "2 - i vale $i"
+done
+
